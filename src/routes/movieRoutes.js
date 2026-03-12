f4d133b31c40886432aef7a3c7142ff1c532b19b
Ahmed Elarosi <cabry95@protonmail.com>
2026-03-12 03:57:14 +0100

    add movie route

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -1,1 +1,9 @@
 import express from "express";
+
+const router = express.Router();
+
+router.get("/hi", (req, res) => {
+    res.json({ message: `HIIIIIIIIIIIIIIIIIII` });
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 
+// Import Routes
+import movieRoutes from "./routes/movieRoutes.js";
+
 const app = express();
 
 app.get("/hi", (req, res) => {
     res.json({ message: "Hallo Liebe Kunden" });
 });
+
+//API Routs
+app.use("/movies", movieRoutes);
 
 const PORT = 5555;
 const server = app.listen(PORT, () => {
