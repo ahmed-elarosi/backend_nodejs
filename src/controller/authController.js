494981acf484071a5ab8ff4546ec224e9ec4176b
Ahmed Elarosi <cabry95@protonmail.com>
2026-03-15 01:50:57 +0100

    add hash password

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,4 +1,5 @@
 import { prisma } from "../config/db.js";
+import bcrypt from "bcryptjs";
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -10,8 +11,13 @@ const register = async (req, res) => {
     });
 
     if (userExists) {
-        return res.status(400).json({ error: "User already exists with this email" });
+        return;
+        res.status(400).json({ error: "User already exists with this email" });
     }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
 };
 
 export { register };
