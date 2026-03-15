d076d609fb1be7357d76c352c0f386eaf8b4957a
Ahmed Elarosi <cabry95@protonmail.com>
2026-03-15 01:46:18 +0100

    check if user exists

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,17 @@
+import { prisma } from "../config/db.js";
+
 const register = async (req, res) => {
-    res.json({ message: "Sign Up" });
+    const { name, email, password } = req.body;
+    res.json(body);
+
+    // check if user exists
+    const userExists = await prisma.user.findUnique({
+        where: { email: email },
+    });
+
+    if (userExists) {
+        return res.status(400).json({ error: "User already exists with this email" });
+    }
 };
 
 export { register };
